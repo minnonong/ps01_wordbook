baf9916477f9bc48d08d738899ed3c2dcbacb0aa
minnonong <minnonong@gmail.com>
2017-09-02 16:10:03 +0900

    Add templates, Modified Views

---
+++ wordbook/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.word_list, name='word_list'),
+]
--- wordbook/views.py
+++ wordbook/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Word
 
-# Create your views here.
+def word_list(request):
+    words = Word.objects.filter(reg_date__lte = timezone.now()).order_by('reg_date')
+    return render(request, 'wordbook/word_list.html',{'words': words})
