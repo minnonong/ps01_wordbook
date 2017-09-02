0005661b9a1f85d95f7685f7f88f8656c07717ed
minnonong <minnonong@gmail.com>
2017-09-02 20:12:34 +0900

    Add CRUD_C

--- wordbook/urls.py
+++ wordbook/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.word_list, name='word_list'),
+    url(r'^word/new/$', views.word_new, name='word_new'),
 ]
--- wordbook/views.py
+++ wordbook/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Word
+from .forms import WordForm
 
 def word_list(request):
     words = Word.objects.filter(reg_date__lte = timezone.now()).order_by('reg_date')
     return render(request, 'wordbook/word_list.html',{'words': words})
+
+def word_new(request):
+    if request.method == "POST":
+        form = WordForm(request.POST)
+        if form.is_valid():
+            word = form.save(commit=False)
+            word.author = request.user
+            word.reg_date = timezone.now()
+            word.save()
+            return redirect('word_list')
+    else:
+        form = WordForm()
+    return render(request, 'wordbook/word_edit.html', {'form': form})
